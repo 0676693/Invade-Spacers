078df9dee3ca0e5f740de7c12886d0238806058e
0676693 <0676693@schoolsnet.act.edu.au>
2023-02-08 10:38:33 +1100

    Shooting & Player Movement
    :D

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -1,16 +1,12 @@
 extends KinematicBody2D
 
+var speed = 500
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(true)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var colliidedObject = move_and_collide(Vector2(0, -speed*delta))
