3707046a47b9056f6a719ec806fd811fdaf6c5cd
connor mcfadden <0676693@schoolsnet.act.edu.au>
2023-02-17 12:58:04 +1100

    Added player collision, changed enemy sprites, added countdown and game over.
    I am an extremely cool human being.
    
    :)

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -1,1 +1,14 @@
 extends Control
+export(int) var countdownMax
+var currentTimer 
+
+func _ready():
+	currentTimer = countdownMax
+	$HUD/Countdown.text = str(currentTimer)
+	
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1,0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("Game Over, noob")
