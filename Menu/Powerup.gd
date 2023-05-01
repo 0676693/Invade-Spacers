90d3a469bb43aabbaa0e87ce0bc8474a37cb2e80
connor mcfadden <0676693@schoolsnet.act.edu.au>
2023-05-01 14:44:57 +1000

    Tried to begin implementing powerups

---
+++ Menu/Powerup.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+export var velocity = Vector2()
+export var spriteSize = int()
+
+func _ready():
+	pass 
+	
+	
+func _process(delta):
+	translate(velocity * delta)
+	if get_position().y - spriteSize >= get_viewport_rect().size.y:
+		queue_free()
+		
+func _on_powerup_body_entered(body):
+		
